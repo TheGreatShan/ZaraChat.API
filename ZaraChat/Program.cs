7aff27babd5d1554b6609f47d4d23bc088139510
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-05-11 19:17:18 +0200

    /version returns version

--- ZaraChat/Program.cs
+++ ZaraChat/Program.cs
@@ -8,41 +8,19 @@ public class MyClass
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
+        
+        app.UseSwagger();
+        app.UseSwaggerUI();
         app.UseHttpsRedirection();
 
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        app.MapGet("/weatherforecast", () =>
+        app.MapGet("/version", () =>
             {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        (
-                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            Random.Shared.Next(-20, 55),
-                            summaries[Random.Shared.Next(summaries.Length)]
-                        ))
-                    .ToArray();
-                return forecast;
+               return "v0.0.1";
             })
-            .WithName("GetWeatherForecast")
+            .WithName("Version")
             .WithOpenApi();
 
         app.Run();
     }
-}
-
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
